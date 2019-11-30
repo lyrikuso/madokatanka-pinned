025fba71885a4d96ab4a4f820a94ba6311d824f2
paithiov909 <am11.44@gmail.com>
2019-12-01 07:50:42 +0900

    generate docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,4 +1,4 @@
-{
+define({
   "name": "madokatanka",
   "version": "1.0.3",
   "description": "My wicked Nuxt.js project",
@@ -7,8 +7,8 @@
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-11-30T08:36:37.487Z",
+    "time": "2019-11-30T22:49:52.827Z",
     "url": "http://apidocjs.com",
     "version": "0.18.0"
   }
-}
+});
