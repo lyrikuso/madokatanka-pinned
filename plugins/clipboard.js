e339c276ee23b44d55f2f5a453b1fd6203b9a92a
Kato Akiru <am11.44@gmail.com>
2020-06-16 16:05:24 +0900

    fix clipboard

--- plugins/clipboard.js
+++ plugins/clipboard.js
@@ -5,17 +5,19 @@ export default {
     };
   },
   mounted() {
-    this.cb = new ClipboardJS(".clip");
-    this.cb.on("success", (e) => {
-      if (process.client) {
-        this.$vs.notify({
-          time: 4000,
-          title: "クリップボードにコピーしました",
-          text: e.text,
-          color: "primary",
-          icon: "info",
-        });
-      }
-    });
+    if (_.isUndefined(this.cb)) {
+      this.cb = new ClipboardJS(".clip");
+      this.cb.on("success", (e) => {
+        if (process.client) {
+          this.$vs.notify({
+            time: 4000,
+            title: "クリップボードにコピーしました",
+            text: e.text,
+            color: "primary",
+            icon: "info",
+          });
+        }
+      });
+    }
   },
 };
